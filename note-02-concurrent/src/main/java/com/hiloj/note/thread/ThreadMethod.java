db89485e7fdfdd2336df40b88bd08b3c812423b0
hiloj <hilojava@163.com>
2020-04-22 11:14:16 +0800

    添加线程等待与唤醒

--- note-02-concurrent/src/main/java/com/hiloj/note/thread/ThreadMethod.java
+++ note-02-concurrent/src/main/java/com/hiloj/note/thread/ThreadMethod.java
@@ -4,18 +4,18 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 线程方法：
- *  线程休眠：Thread.sleep(time)或TimeUnit.SECONDS.sleep(time)
- *  线程启动：thread.start()
- *  存活线程数：Thread.activeCount()
- *  当前线程：Thread.currentThread()
- *  线程组：thread.getThreadGroup()
- *  线程优先级：thread.getPriority()
- *  线程是否存活：thread.isAlive()
- *  线程是否守护线程：thread.isDaemon()
- *  中断线程：thread.interrupt()
- *  线程是否中断，清除中断标志位：Thread.interrupted()
- *  线程是否中断，不清除中断标志位：thread.isInterrupted()
- *  线程插队：thread.join()
+ * 线程休眠：Thread.sleep(time)或TimeUnit.SECONDS.sleep(time)
+ * 线程启动：thread.start()
+ * 存活线程数：Thread.activeCount()
+ * 当前线程：Thread.currentThread()
+ * 线程组：thread.getThreadGroup()
+ * 线程优先级：thread.getPriority()
+ * 线程是否存活：thread.isAlive()
+ * 线程是否守护线程：thread.isDaemon()
+ * 中断线程：thread.interrupt()
+ * 线程是否中断，清除中断标志位：Thread.interrupted()
+ * 线程是否中断，不清除中断标志位：thread.isInterrupted()
+ * 线程插队：thread.join()
  */
 public class ThreadMethod {
     public static void main(String[] args) {
@@ -25,12 +25,63 @@ public class ThreadMethod {
         // threadJoin();
         // 守护线程
         // threadDeamon();
-        threadInterrupt();
+        // 线程中断
+        // threadInterrupt();
+        // 线程等待及唤醒
+        threadWaitAndNotify();
+    }
+
+    // 线程等待及唤醒
+    private static void threadWaitAndNotify() {
+        Object o = new Object();
+        new Thread(() -> {
+            synchronized (o) {
+                for (int i = 0; i < 5; i++) {
+                    if (i == 3) {
+                        try {
+                            System.out.println("线程【" + Thread.currentThread().getName() + "】\t 即将进入等待状态");
+                            o.wait();
+                            System.out.println("线程【" + Thread.currentThread().getName() + "】\t 被唤醒了");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("线程【" + Thread.currentThread().getName() + "】\t 打印当前值为：" + i);
+                }
+                System.out.println("线程【" + Thread.currentThread().getName() + "】\t 运行结束，即将唤醒其他等待线程");
+                o.notifyAll();
+            }
+        }, "Thread-A").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            synchronized (o) {
+                for (int i = 0; i < 5; i++) {
+                    if (i == 3) {
+                        System.out.println("线程【" + Thread.currentThread().getName() + "】\t 即将唤醒其他等待线程");
+                        o.notifyAll();
+                        try {
+                            System.out.println("线程【" + Thread.currentThread().getName() + "】\t 即将进入等待状态");
+                            o.wait();
+                            System.out.println("线程【" + Thread.currentThread().getName() + "】\t 被唤醒了");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("线程【" + Thread.currentThread().getName() + "】\t 打印当前值为：" + i);
+                }
+            }
+        }, "Thread-B").start();
     }
 
     // 线程中断
     private static void threadInterrupt() {
-        new Thread(()->{
+        new Thread(() -> {
             for (int i = 0; i < 4; i++) {
                 if (i == 1) {
                     // 当前线程中断
@@ -40,9 +91,9 @@ public class ThreadMethod {
                 System.out.println("线程【" + Thread.currentThread().getName() + "】\t 当前值为：" + i);
                 System.out.println("线程【" + Thread.currentThread().getName() + "】\t Thread.interrupted()为：" + Thread.interrupted());
             }
-        },"Thread-A").start();
+        }, "Thread-A").start();
 
-        new Thread(()->{
+        new Thread(() -> {
             for (int i = 0; i < 4; i++) {
                 if (i == 1) {
                     // 当前线程中断
@@ -52,17 +103,20 @@ public class ThreadMethod {
                 System.out.println("线程【" + Thread.currentThread().getName() + "】\t 当前值为：" + i);
                 System.out.println("线程【" + Thread.currentThread().getName() + "】\t Thread.currentThread().isInterrupted()为：" + Thread.currentThread().isInterrupted());
             }
-        },"Thread-B").start();
+        }, "Thread-B").start();
     }
 
     // 设置守护线程
     private static void threadDeamon() {
         // 添加退出钩子
-        Runtime.getRuntime().addShutdownHook(new Thread(()-> System.out.println("虚拟机成功退出")));
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println("虚拟机成功退出")));
         Thread thread = new Thread(() -> {
             while (true) {
                 try {
-                    TimeUnit.MILLISECONDS.sleep(500);} catch(InterruptedException e) {e.printStackTrace();}
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println("线程【" + Thread.currentThread().getName() + "】\t 守护线程在运行");
             }
         }, "Thread-deamon-1");
@@ -71,7 +125,11 @@ public class ThreadMethod {
         // 设置守护线程
         thread.setDaemon(true);
         thread.start();
-        try {TimeUnit.SECONDS.sleep(3);} catch(InterruptedException e) {e.printStackTrace();}
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         System.out.println("主线程即将推出");
     }
@@ -87,7 +145,7 @@ public class ThreadMethod {
             System.out.println("线程【" + Thread.currentThread().getName() + "】\t 结束执行");
         }, "Thread-join-1");
 
-        new Thread(()->{
+        new Thread(() -> {
             System.out.println("线程【" + Thread.currentThread().getName() + "】\t 开始执行");
             try {
                 thread.join();
@@ -95,13 +153,13 @@ public class ThreadMethod {
                 e.printStackTrace();
             }
             System.out.println("线程【" + Thread.currentThread().getName() + "】\t 结束执行");
-        },"Thread-join-2").start();
+        }, "Thread-join-2").start();
 
         thread.start();
     }
 
     private static void BaseMethod() {
-        Thread thread1 = new Thread(new ThreadGroup("Thread-1-group"),() -> {
+        Thread thread1 = new Thread(new ThreadGroup("Thread-1-group"), () -> {
             try {
                 // 线程休眠
                 TimeUnit.SECONDS.sleep(1);
@@ -110,9 +168,9 @@ public class ThreadMethod {
                 // 当前线程
                 Thread thread = Thread.currentThread();
                 // 当前线程名称
-                System.out.println("线程名称为：" +thread.getName());
+                System.out.println("线程名称为：" + thread.getName());
                 // 当前线程组
-                System.out.println("线程组为：" +thread.getThreadGroup());
+                System.out.println("线程组为：" + thread.getThreadGroup());
                 // 当前线程优先级
                 System.out.println("线程优先级为：" + thread.getPriority());
                 // 线程是否存活
